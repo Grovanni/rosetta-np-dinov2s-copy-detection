b6f87615684c110f90157a927c194cbacf69c049
Amorphe160 <161823611+Grovanni@users.noreply.github.com>
2026-07-31 13:22:17 +0200

    docs: refine Rosetta public presentation

--- src/rosetta_copy/__init__.py
+++ src/rosetta_copy/__init__.py
@@ -1,7 +1,6 @@
-"""Rosetta NP compact image-copy descriptors."""
+"""Rosetta compact image-copy descriptors."""
 
 from .model import RosettaEncoder, download_weights, list_variants
 
 __all__ = ["RosettaEncoder", "download_weights", "list_variants"]
 __version__ = "1.0.0"
-
